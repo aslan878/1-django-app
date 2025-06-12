114e7b68ebede91978c299a76f2f2305bec0469c
Aslan <aslan@MacBook-Pro-Aslan.local>
2025-06-12 10:52:51 +0500

    Обновил и шаблоны

--- aslanweb/wsgi.py
+++ aslanweb/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aslanweb.settings')
 
 application = get_wsgi_application()
+
+from whitenoise import WhiteNoise
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+
